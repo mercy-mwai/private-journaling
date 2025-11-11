57de96f524a6060aa3d472021d6a34a031614264
mercy-mwai <mercyneemam@gmail.com>
2025-11-11 13:54:31 +0300

    Todos request

---
+++ app/Http/Requests/StoreTodoRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTodoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'date'=> 'required|date_format:Y-m-d',
+            'text'=>'required|string|max:500',
+            'completed'=>'nullable|boolean'
+        ];
+    }
+}
