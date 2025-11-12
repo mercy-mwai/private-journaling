6163a0656d778ac74e733b4ac42875dcf19089df
mercy-mwai <mercyneemam@gmail.com>
2025-11-12 10:29:58 +0300

    Planner entry controller and Routes

--- app/Http/Controllers/PlannerEntryController.php
+++ app/Http/Controllers/PlannerEntryController.php
@@ -2,20 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PlannerEntry;
 use Illuminate\Http\Request;
 use App\Models\PlannerMetadata;
+use App\Http\Requests\StorePlannerEntryRequest;
 
 class PlannerEntryController extends Controller
 {
-    public function store(Request $request){
-        $validatedEntries= $request->validate([
-            'main_focus'=> 'string|max:255|required',
-            'goals'=> 'string|nullable',
-            'mood'=> 'string|nullable|max:100',
+    public function getByWeek($startDate) :JsonResponse
+    {
+       $entries= PlannerEntry:: where('user_id', auth()->id())
+        ->where('date', '>=', $startDate)
+        ->where('date', '<', date('Y-m-d',strtotime($startDate. '+7 days')))
+        ->orderBy('date')
+        ->orderBy('time')
+        ->get();
 
-        ]);
-        $metadata = PlannerMetadata::create($validatedEntries);
-        
+
+         return response()->json([
+        'entries'=>$entries
+    ]);
     }
+    public function getByDay($date) :JsonResponse
+    {
+        $entries =PlannerEntry::where('user_id',auth()->id())
+        ->where('date', $date)
+        ->orderBy('time')
+        ->get();
+
+        return response()->json([
+            'entries'=>$entries
+        ]);
+    }
+
+    public function Store(StorePlannerEntryRequest $request) :JsonResponse
+    {
+        $entries= PlannerEntry::create([
+            'user_id'=>auth()->id(),
+            'date'=>$request->date,
+            'time'=>$request->time,
+            'activity'=>$request->activity,
+            'day_of_week'=>$request->day_of_week
+        ]);
+        return response()->json([
+            'message'=> 'Activty added successfully',
+            'data'=>$entries
+        ]);
+    }
+    public function Update(StorePlannerEntryRequest $request, $id):JsonResponse
+    {
+        $entry =PlannerEntry::where('user_id', auth()->id())
+        ->findOrFail($id);
+
+        $updateEntry=update([
+            'time'=>$request->time,
+            'activity'=>$request->activity,
+            'day_of_week'=>$request->day_of_week
+        ]);
+
+        return response()->json([
+            'message'=> 'Activity updated Successfully',
+            'data'=>$updateEntry
+        ]);
+    }
+  public function Destroy($id):JsonResponse
+  {
+    $entry= PlannerEntry::where('user_id', auth()->id())
+    ->findOrFail($id);
+   $entry->delete();
+
+   return response()->json([
+    'message'=> 'Activity Deleted Successfully',
+
+   ]);
+  }
     
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,10 @@ Route::middleware('auth:sanctum')->prefix('auth')->group(function(){
 Route::post('/logout', [AuthController::class, 'Logout']);
 });
 
-Route::post('/planner-metadata/{date}', [PlannerEntryController::class, '' ]);
+//planner entries Routes
+
+Route::get('/planner_entries/week/$startDate', [PlannerEntryController::class, 'getByWeek']);
+Route::get ('/planner_entries/day/$date', [PlannerEntryController::class, 'getByDay']);
+Route::post('/planner_entries', [PlannerEntryController::class, 'Store']);
+Route::put('/planner_entries/update', [PlannerEntryController::class, 'Update']);
+Route::delete('planner_entries/delete', [PlannerEntryController::class, 'Destroy']);
